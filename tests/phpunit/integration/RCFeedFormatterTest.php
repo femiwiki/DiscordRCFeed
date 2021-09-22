bac48396ba22b3ce9e978186dd1861822cb91667
lens0021 <lorentz0021@gmail.com>
2021-09-23 01:32:45 +0900

    Remove some tests

--- tests/phpunit/integration/RCFeedFormatterTest.php
+++ tests/phpunit/integration/RCFeedFormatterTest.php
@@ -57,34 +57,4 @@ class RCFeedFormatterTest extends MediaWikiIntegrationTestCase {
 			)
 		);
 	}
-
-	/**
-	 * @covers \MediaWiki\Extension\DiscordRCFeed\RCFeedFormatter::pushDiscordNotify
-	 */
-	public function testPushDiscordNotify() {
-		$formatter = $this->formatter;
-		$this->assertFalse( $formatter->pushDiscordNotify( '', null, 'article_saved' ) );
-
-		$this->setMwGlobals( 'wgDiscordRCFeedIncomingWebhookUrl', 'http://127.0.0.1/webhook' );
-		$this->assertNull( $formatter->pushDiscordNotify( '', null, 'article_saved' ) );
-
-		$this->setMwGlobals( 'wgDiscordRCFeedSendMethod', 'random' );
-		$this->assertFalse( $formatter->pushDiscordNotify( '', null, 'article_saved' ) );
-	}
-
-	public function testDiscordRCFeed() {
-		$this->setMwGlobals( [
-			'wgDiscordRCFeedIncomingWebhookUrl' => 'https:// webhook',
-			'wgServer' => 'https://foo.bar'
-		] );
-		$ct = 1;
-		$user = new User();
-		$user->setName( 'EditTest' );
-		$user->addToDatabase();
-		$this->editPage( 'Edit Test', str_repeat( 'lorem', $ct++ ), '', NS_MAIN, $user );
-
-		// phpcs:ignore Generic.Files.LineLength.TooLong
-		$regex = '~📄 \[EditTest\]\(https://foo\.bar/index\.php/User:EditTest\) \(\[block\]\(https://foo\.bar/index\.php(\?title=|/)Special:Block/EditTest\) \| \[groups\]\(https://foo\.bar/index\.php(\?title=|/)Special(%3A|:)UserRights(&user=|/)EditTest\) \| \[talk\]\(https://foo\.bar/index\.php(\?title=|/)User_talk:EditTest\) \| \[contribs\]\(https://foo\.bar/index\.php(\?title=|/)Special:Contributions/EditTest\)\) has created article \[Edit Test\]\(https://foo\.bar/index\.php(\?title=|/)Edit(%20|_)Test\) \(\[edit\]\(https://foo\.bar/index\.php\?title=Edit(%20|_)Test&action=edit\) \| \[delete\]\(https://foo\.bar/index\.php\?title=Edit(%20|_)Test&action=delete\) \| \[history\]\(https://foo\.bar/index\.php\?title=Edit(%20|_)Test&action=history\)\)  \(5 bytes\)~';
-		$this->assertRegExp( $regex, RCFeedFormatter::$lastMessage );
-	}
 }
