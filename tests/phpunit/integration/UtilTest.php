73b041166fe115c5e1c43a22c1dda96bdbea6a0d
lens0021 <lorentz0021@gmail.com>
2021-09-27 20:26:56 +0900

    Add UtilTest

--- tests/phpunit/integration/UtilTest.php
+++ tests/phpunit/integration/UtilTest.php
@@ -4,6 +4,7 @@ namespace MediaWiki\Extension\DiscordRCFeed\Tests\Integration;
 
 use MediaWiki\Extension\DiscordRCFeed\Util;
 use MediaWikiIntegrationTestCase;
+use MessageCache;
 use RequestContext;
 
 /**
@@ -63,5 +64,41 @@ class UtilTest extends MediaWikiIntegrationTestCase {
 	public function testGetContentLanguageContext() {
 		$ctx = Util::getContentLanguageContext();
 		$this->assertInstanceOf( RequestContext::class, $ctx );
+	}
+
+	/**
+	 * @covers \MediaWiki\Extension\DiscordRCFeed\Util::msgText
+	 */
+	public function testConvertUserName() {
+		$msgMap = [
+			'en' => [
+				'test-prefix-block-block' => 'A blocked B.',
+			],
+			'ko' => [
+				'test-prefix-block-block' => 'A가 B를 차단했습니다.',
+			],
+		];
+		$mock = $this->createMock( MessageCache::class );
+		$mock->method( 'get' )
+			->will( $this->returnCallback(
+				static function ( $key, $_, $lang ) use ( $msgMap ) {
+					return $msgMap[$lang->getCode()][$key] ?? '';
+				}
+			)
+		);
+		$mock->method( 'transform' )
+			->will( $this->returnArgument( 0 ) );
+
+		$this->setService( 'MessageCache', $mock );
+		// $cache = \MediaWiki\MediaWikiServices::getInstance()->getMessageCache();
+		// fwrite( STDERR, $cache->get( 'test-prefix-block-block', null, null ) );
+		// fwrite( STDERR, get_class( $cache ) );
+		// $this->assertSame( '$1 blocked $2.', $cache->get( 'test-prefix-block-block', null, null ) );
+		$msg = Util::msgText( 'test-prefix-block-block' );
+		$this->assertNotEmpty( $msg, 'should return not empty value when valid key is given' );
+		$this->assertIsString( $msg, 'should return string value' );
+		$this->setMwGlobals( 'wgLanguageCode', 'ko' );
+		$msg = Util::msgText( 'test-prefix-block-block' );
+		$this->assertSame( 'A가 B를 차단했습니다.', $msg, 'should return message in the content language' );
 	}
 }
